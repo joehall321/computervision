970c33f4aecbbb6e383aed3dc8a3e66bee0cc82a
joehall321 <halljoe312@gmail.com>
2023-04-02 21:05:04 +0100

    modified:   task3/task3.py

--- task3/task3.py
+++ task3/task3.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import MeanShift, KMeans, estimate_bandwidth
 from os import listdir
 from tqdm import tqdm
+import itertools
 
 task2_dataset = "/datasets/task2_images/"
     
@@ -92,39 +93,80 @@ def findBandWidthRange(path, SIFT_params=None):
         bandwidths.append(bandwidth)
     return int(min(bandwidths)), int(max(bandwidths))
 
-def GridSearchBandwidthMeanshift(path, SIFT_params=None):
-    # Get bandwidth ranges 
-    bandwidth_range=findBandWidthRange(path, SIFT_params)
+def tryDifferentGaussians(paths):
+    for path in paths:
+        print("Dataset:"+path)
+        print()
+        # Object images
+        object_images = [f for f in listdir(path)]
+        for k in range(3,16,2):
+            for idx in range(len(object_images)):
+                # Read image
+                img = cv2.imread(path+object_images[idx])
+                blurred = preprocess(img,k)
+                fig, axes = plt.subplots(nrows=1, ncols=2)
+                # Marking keypoints on the image using circles
+                axes[0].imshow(img)
+                axes[1].imshow(cv2.cvtColor(blurred, cv2.COLOR_GRAY2RGB))
+                axes[0].title.set_text("Original Image")
+                axes[1].title.set_text("Gaussian Blur k="+str(k))
+                plt.show()
 
-    # Object images and annotations
-    object_images = [f for f in listdir(path)]
-    object_images.sort()
-    annotation = path+"../annotations/"
-    object_annotations = [f for f in listdir(annotation)]
-    object_annotations.sort()
-    print("Bandwidth range of images using sklearn.cluster.estimate_bandwidth:",bandwidth_range)
+
+def GridSearchBandwidthMeanshift(paths, plot_errors=False):
+    object_images=[]
+    object_annotations=[]
+    for path in paths:
+        print("Dataset:"+path)
+        # Object images and annotations
+        ims = [path+f for f in listdir(path)]
+        ims.sort()
+        annotation = path+"../annotations/"
+        ans = [annotation+f for f in listdir(annotation)]
+        ans.sort()
+
+        object_images = object_images + ims
+        object_annotations = object_annotations + ans
 
     # Create SIFT detector
     sift=cv2.SIFT_create()
-    if SIFT_params is not None:
-        sift.setEdgeThreshold(SIFT_params[0])
-        sift.setNOctaveLayers(SIFT_params[1])
     
-    # Gridsearch 
-    print()
-    print("FINDING OPTIMAL BANDWIDTH FOR MEANSHIFT")
+    # Reading images and annotations
+    images=[]
+    n_objects=[]
+    for idx in range(len(object_images)):
+        # Read image
+        images.append(preprocess(cv2.imread(object_images[idx]),9))
+
+        # Read annotation
+        with open(object_annotations[idx]) as f:
+            lines = f.readlines()
+        n_objects.append(len(lines))
+
+    # Gridsearch parameters
     accuracies = []
-    # constants = [n/100 for n in range(1,101,1)]
-    constants = [0.5]
-    for constant in tqdm(constants):
+    #constants = [n/100 for n in range(51,64)]
+    constants = [n/100 for n in range(50,61)]
+    edgesThresholds = [n for n in range(2,16,2)]
+    octaves = [n for n in range(2,7)]
+    sigmas = [n/10 for n in range(5,21)]
+    #parameters = list(itertools.product(constants,octaves,sigmas,edgesThresholds))
+    parameters = [(0.51, 5, 1.2, 4)] 
+
+    print()
+    print("FINDING OPTIMAL BANDWIDTH FOR MEANSHIFT OBJECT CLUSTERING")
+    counter = 0
+    for (constant,octave,sigma,edgesThreshold) in tqdm(parameters):
         clustering_accuracy = []
-        for idx in range(len(object_images)):
-            # Read image
-            img = cv2.imread(path+object_images[idx])
-            # Read annotation
-            with open(annotation+object_annotations[idx]) as f:
-                lines = f.readlines()
-            n_objects=len(lines)
+
+        # Set SIFT params
+        sift.setNOctaveLayers(octave)
+        sift.setSigma(sigma)
+        sift.setEdgeThreshold(edgesThreshold)
+        
+        for idx in range(len(images)):
+            img = images[idx]
+            n_img_objects = n_objects[idx]
 
             # SIFT
             kp = sift.detect(img,None)
@@ -134,9 +176,9 @@ def GridSearchBandwidthMeanshift(path, SIFT_params=None):
             bandwidth = estimate_bandwidth(kps_xy)*constant
             labels_meanshift = meanshift(kps_xy,bandwidth=bandwidth)
             n_clusters = len(np.unique(labels_meanshift))
-            clustering_accuracy.append(n_objects==n_clusters)
+            clustering_accuracy.append(n_img_objects==n_clusters)
 
-            if n_objects!=n_clusters:
+            if n_img_objects!=n_clusters and plot_errors:
                 fig, axes = plt.subplots(nrows=1, ncols=2)
                 plotClusters(axes[1],labels_meanshift,"Meanshift",kps_xy)
                 # Marking keypoints on the image using circles
@@ -151,59 +193,124 @@ def GridSearchBandwidthMeanshift(path, SIFT_params=None):
 
         clustering_correctness = len([r for r in clustering_accuracy if r is True])/len(clustering_accuracy)
         accuracies.append(clustering_correctness)
+        # counter+=1
+        # print("Counter:",counter)
+        # if counter == 3:
+        #     break
+
     print("DONE")
     print()
     max_accuracy = max(accuracies)
-    max_constants = [constants[idx] for idx in [i for i, j in enumerate(accuracies) if j == max_accuracy]]
-    print("Optimal constants:",max_constants, "with accuracy:",max_accuracy)
+    max_parameters = [parameters[idx] for idx in [i for i, j in enumerate(accuracies) if j == max_accuracy]]
+    print("Optimal constants:",max_parameters, "with accuracy:",max_accuracy)
     exit()
-        
 
-        
+def getObjectDatabase(path, gaussian_k, SIFT_params):
+    objects = [f for f in listdir(path)]
+    SIFT_n_octaves, SIFT_sigma, SIFT_edge_threshold = SIFT_params
 
-    
+    sift=cv2.SIFT_create()
+    sift.setEdgeThreshold(SIFT_edge_threshold)
+    sift.setNOctaveLayers(SIFT_n_octaves)
+    sift.setSigma(SIFT_sigma)
+
+    object_database={}
+    for object in objects:
+
+        name = object[4:len(object)-4]
+        img = cv2.imread(path+object)
+        blurred = preprocess(img,gaussian_k)
+        kp, desc = sift.detectAndCompute(blurred,None)
+        object_database[name]=(img,kp,desc)
+        
+    return object_database
 
 print()
-path="datasets/task2_images/images/"
+image_paths="datasets/task2_images/images/","datasets/task3_images/images/"
+objects_path="datasets/objects/"
 
-GridSearchBandwidthMeanshift(path)
+#GridSearchBandwidthMeanshift(paths, plot_errors=True)
 
-object_images = [f for f in listdir(path)]
-for file in object_images:
-    img = cv2.imread(path+file)   
+# Parameters
+gaussain_k=9
+ms_bandwidth_constant = 0.51
+SIFT_n_octaves = 5
+SIFT_sigma = 1.2
+SIFT_edge_threshold = 4
 
-    # show image with matplotlib
-    # plt.imshow(img)
+#Creating SIFT detector
+print("Gaussian Blur k:",gaussain_k)
+print("SIFT EdgeThreshold",SIFT_edge_threshold)
+print("SIFT NOctaveLayers",SIFT_n_octaves)
+print("SIFT Sigma",SIFT_sigma)
+print("Meanshift bandwidth constant:",ms_bandwidth_constant)
+sift=cv2.SIFT_create()
+sift.setEdgeThreshold(SIFT_edge_threshold)
+sift.setNOctaveLayers(SIFT_n_octaves)
+sift.setSigma(SIFT_sigma)
 
-    # Applying preprocessing 
-    gray = preprocess(img, gaussain_k=9)
+# object_database[name]=(img,kp,desc)
+object_database = getObjectDatabase(objects_path, gaussain_k, (SIFT_n_octaves,SIFT_sigma,SIFT_edge_threshold))
 
-    # Applying SIFT detector
-    sift=cv2.SIFT_create()
-    sift.setEdgeThreshold(10)
-    sift.setNOctaveLayers(6)
-    print("Default EdgeThreshold",sift.getEdgeThreshold())
-    print("Default NFeatures",sift.getNFeatures())
-    print("Default NOctaveLayers",sift.getNOctaveLayers())
-    print("Default Sigma",sift.getSigma())
-    kp = sift.detect(img,None)
-    kps_xy = [pt.pt for pt in kp]
+for path in image_paths:
 
-    # Meanshift 
-    labels_meanshift = meanshift(kps_xy)
-    bandwidth=estimate_bandwidth(kps_xy)
-    print("Meanshift bandwidth:",bandwidth)
+    object_images = [f for f in listdir(path)]
+    annotation = path+"../annotations/"
+    annotations = [f for f in listdir(annotation)]
 
-    # Marking keypoints on the image using circles
-    img=cv2.drawKeypoints(gray,
-                        kp,
-                        img,
-                        flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-    
-    fig, axes = plt.subplots(nrows=1, ncols=2)
-    plotClusters(axes[1],labels_meanshift,"Meanshift",kps_xy)
-    axes[0].imshow(img)
-    axes[1].imshow(img)
-    axes[0].title.set_text("SIFT Keypoints")
-    print()
-    plt.show()
+    for idx in range(len(object_images)):
+
+        img = cv2.imread(path+object_images[idx])   
+
+        # Read annotation
+        with open(annotation+annotations[idx]) as f:
+            lines = f.readlines()
+        n_objects = len(lines)
+
+        # show image with matplotlib
+        # plt.imshow(img)
+
+        # Applying preprocessing 
+        blurred = preprocess(img, gaussain_k)
+
+        # Applying SIFT detector
+        kp, desc = sift.detectAndCompute(blurred,None)
+        kps_xy = [pt.pt for pt in kp]
+
+        # Meanshift 
+        ms_bandwidth=estimate_bandwidth(kps_xy)*ms_bandwidth_constant
+        labels_meanshift = meanshift(kps_xy,bandwidth=ms_bandwidth)
+        
+        visual_words = {}
+        for idx in range(len(labels_meanshift)):
+            label = labels_meanshift[idx]
+            visual_words.setdefault(label,[])
+            visual_words.get(label).append(desc[idx])
+        
+        #feature matching
+        bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck=True)
+
+        # object_database[name]=(img,kp,desc)
+        print(object_database["trash"])
+        print(np.array(visual_words[0]))
+        for object in visual_words:
+            matches = bf.match(object_database["trash"][2],desc)
+            matches = sorted(matches, key = lambda x:x.distance)
+            img3 = cv2.drawMatches(object_database["trash"][0], object_database["trash"][1], 
+                                   img, kp, matches[:50], img, flags=2)
+            plt.imshow(img3),plt.show()
+        exit()
+        
+        # Marking keypoints on the image using circles
+        # img=cv2.drawKeypoints(blurred,
+        #                     kp,
+        #                     img,
+        #                     flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        
+        # fig, axes = plt.subplots(nrows=1, ncols=2)
+        # plotClusters(axes[1],labels_meanshift,"Meanshift",kps_xy)
+        # axes[0].imshow(img)
+        # axes[1].imshow(img)
+        # axes[0].title.set_text("SIFT Keypoints")
+        # print()
+        # plt.show()
